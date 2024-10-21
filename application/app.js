bbc740b4f48f83ca61e80b27b10f0237c82a3a67
Ruanhenryy <caruaru288@gmail.com>
2024-10-20 21:39:55 -0300

    conclusão do carrossel

---
+++ application/app.js
@@ -0,0 +1,17 @@
+//Carrossel principal
+document.querySelector('#radio1').checked = true
+let contador = 1
+
+setInterval(() => {
+    proximaImg()
+},2000)
+
+function proximaImg() {
+    contador++;
+
+    if(contador > 5){
+        contador = 1
+    }
+
+    document.getElementById('radio'+contador).checked = true
+}
