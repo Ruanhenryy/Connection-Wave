d9345a3c565134e7fca265d2df239e664df53036
Ruanhenryy <caruaru288@gmail.com>
2024-12-01 22:51:03 -0300

    Aplicando bootstrap

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ function loading(){
     document.getElementsByTagName('header')[0].style.display = 'block'
 }
 
+//Menu
+//const menu = document.querySelector('.icon')
+//function color(){
+    //menu.style.color = 'blue'
+//}
+
+const menu =  document.querySelector('.nav-menu')
+const contentMenu =  document.querySelector('.nav-mobile')
+const main = document.querySelector('.main')
+
+menu.addEventListener('click' , () => {
+    contentMenu.classList.toggle('nav-mobile-menu')
+    main.classList.toggle('opaco')
+})
+
 //Slider principal
 
 document.querySelector('#radio1').checked = true
