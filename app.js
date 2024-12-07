875b976dfa616c48d411b6093f6f8b784497217f
Ruanhenryy <caruaru288@gmail.com>
2024-12-07 18:55:48 -0300

    remodelação pagina inicial

--- app.js
+++ app.js
@@ -26,12 +26,15 @@ menu.addEventListener('click' , () => {
 
 //Slider principal
 
+const next = document.querySelector('.carousel-control-next')
+const prev = document.querySelector('.carousel-control-prev')
 document.querySelector('#radio1').checked = true
+
 let contador = 1
 
 setInterval(() => {
     proximaImg()
-},1000)
+},4000)
 
 function proximaImg() {
     contador++;
@@ -42,3 +45,21 @@ function proximaImg() {
 
     document.getElementById('radio'+contador).checked = true
 }
+
+function anteriorImg(){
+    contador--;
+
+    if(contador < 0){
+        contador = 5
+    }
+
+    document.getElementById('radio'+contador).checked = true
+}
+
+next.addEventListener('click', () => {
+    proximaImg()
+})
+
+prev.addEventListener('click', () => {
+    anteriorImg()
+})
